969595271a85340b8c843f4900aee3439d1f2158
Choi-yunho <choiyh2001@naver.com>
2026-03-12 17:08:47 +0900

    0312 02_node/01_nodejs기초

---
+++ 02_Node/01_basic/01_nodejs기초/07_app-1.js
@@ -0,0 +1,8 @@
+const user = require('./07_user');
+const hello = require('./07_hello');
+
+
+console.log(user);
+console.log(hello);
+
+hello(user); // 모듈에서 가져온 user 변수와 hello 함수 사용하기
---
+++ 02_Node/01_basic/01_nodejs기초/10_app-6.mjs
@@ -0,0 +1,4 @@
+import { hi, goodbye } from './09_ems/09_greeting-1.mjs';
+
+hi('홍길동');
+goodbye('홍길동');
